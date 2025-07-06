102e467411c9bfeba4f7905a4a3f2aa00a2f6d12
rventurave <rventurave@unsa.edu.pe>
2025-07-06 15:06:21 -0500

    Funcion completada de cambabiar la contrasena

--- domain/models/Usuarios/usuario.py
+++ domain/models/Usuarios/usuario.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 class Usuario:
     def __init__(self):
         self.usuario_id = None
         self.nombres = None
         self.correo = None
-        self.hash_contraseña = None
+        self.hash_contrasena = None
 
-    def cambiar_contraseña(self, hash):
-        pass
+    def cambiar_contrasena(self, nuevo_hash):
+        self.hash_contrasena = nuevo_hash
+        print("Contraseña cambiada correctamente.")
