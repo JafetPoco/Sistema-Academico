09a4b2a050cf2b96e34e26d2dc5382d88934c0fd
rventurave <rventurave@unsa.edu.pe>
2025-07-06 18:26:21 -0500

    CRUD de los usuarios implementado

--- domain/repositories/mysql/usuario_repositorio_impl.py
+++ domain/repositories/mysql/usuario_repositorio_impl.py
@@ -13,3 +13,20 @@ class usuario_repositorio_impl(Base):
     apellido = Column(String(100))
     correo = Column(String(100))
     hash_contraseña = Column(String(256))
+    
+    # CRUD operations
+    def create(self, session, usuario):
+        session.add(usuario)
+        session.commit()
+        return usuario
+    
+    def get(self, session, usuario_id):
+        return session.query(usuario_repositorio_impl).filter_by(usuario_id=usuario_id).first()
+    
+    def update(self, session, usuario_id, **kwargs):
+        session.query(usuario_repositorio_impl).filter_by(usuario_id=usuario_id).update(kwargs)
+        session.commit()
+        
+    def delete(self, session, usuario_id):
+        session.query(usuario_repositorio_impl).filter_by(usuario_id=usuario_id).delete()
+        session.commit()
