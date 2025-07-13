7cb79adc0bdb164b2c3321f80ac97ebc9535bafa
BerlyCS <>
2025-07-13 11:33:03 -0500

    Eliminacion de db.py conflicto con paquetes ya instalados

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from config.config import Config
-from database.db import db  # ← ¡importar desde el archivo nuevo!
 
 from interfaces.controllers.main_controller import main_bp
 from interfaces.controllers.curso_controlador import curso_bp
@@ -17,14 +16,6 @@ def create_app():
     )
 
     app.config.from_object(Config)
-    db.init_app(app)
-
-    # Importar modelos después de db.init_app
-    from domain.models.Notas.curso import Curso
-
-    # Crear tablas dentro del contexto de aplicación
-    with app.app_context():
-        db.create_all()
 
     app.register_blueprint(main_bp)
     app.register_blueprint(curso_bp)
