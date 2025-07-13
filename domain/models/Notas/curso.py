bb3614dd928a2b3991e6cbce3f2a1fd35db25bdf
aler_hp <autademontag@gmail.com>
2025-07-12 20:08:43 -0500

    Agregada definicion de tabla curso

--- domain/models/Notas/curso.py
+++ domain/models/Notas/curso.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from sqlalchemy import Column, Integer, String,ForeignKey
+from domain.models.Profesor import profesor
+from models.base import Base
 
-class Curso:
-    def __init__(self):
-        self.curso_id = None
-        self.nombre = None
-        self.Attribute1 = None
+class curso(Base):
+
+    tablename = 'cursos'
+    curso_id = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    profesor_id = Column(Integer, ForeignKey('profesores.profesor_id'), nullable=False)
+
 
     def agregar_estudiante(self, estudiante):
         pass
