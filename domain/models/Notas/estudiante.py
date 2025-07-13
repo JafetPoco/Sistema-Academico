035d3ca2c0e39833b5d2fc7eb399df67281d34eb
aler_hp <autademontag@gmail.com>
2025-07-12 19:59:49 -0500

    Agregada definicion de tabla Estudiante

--- domain/models/Notas/estudiante.py
+++ domain/models/Notas/estudiante.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class Estudiante:
+from sqlalchemy import Column, Integer, ForeignKey
+from domain.models.base import Base
+
+class estudiante(Base):
+
+    tablename = 'estudiantes'
+    id = Column(Integer, ForeignKey('usuarios.usuario_id'), primary_key=True)
+    padre_id = Column(Integer, ForeignKey('padres.id'), nullable=True)
+    
     def __init__(self):
         self.estudiante_id = None
         self.usuario_id = None
