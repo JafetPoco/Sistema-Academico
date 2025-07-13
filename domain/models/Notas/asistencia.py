7510f16122e8b4509227498c758fea603183a6c5
BerlyCS <>
2025-07-13 11:31:58 -0500

    Solucion a importacion circular

--- domain/models/Notas/asistencia.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-class Asistencia:
-    def __init__(self):
-        self.asistencia_id = None
-        self.estudiante_id = None
-        self.curso_id = None
