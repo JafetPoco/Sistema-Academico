49f5dcd6ebe9f573b51da560d328c80d2a4c7cdb
rventurave <rventurave@unsa.edu.pe>
2025-07-12 20:31:54 -0500

    impremetacion de la funcion ver notas, que me retorna las calificaiones de un estudiante en especifico

--- domain/models/Padre/padre.py
+++ domain/models/Padre/padre.py
@@ -8,5 +8,5 @@ class Padre:
     def agregar_estudiante(self, estudiante):
         pass
 
-    def ver_calificaciones(self, estudiante):
-        pass
+    def ver_calificaciones(self, estudiante_id, calificacion_service):
+        return calificacion_service.obtener_calificaciones_por_estudiante(estudiante_id)
