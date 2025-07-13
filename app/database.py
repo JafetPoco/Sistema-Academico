4d914d48a91245bc2de41550986e319c356ac484
BerlyCS <>
2025-07-13 12:03:12 -0500

    Renombrar namespace

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from domain.models.base import Base
+from service.mysql.base import Base
 
 engine = create_engine('mysql+mysqlconnector://admin:12345@localhost/testdb', echo=True)
 SessionLocal = sessionmaker(bind=engine)
