575b2b9b394644d7adb0a383085561c970683a7f
rventurave <rventurave@unsa.edu.pe>
2025-07-21 00:13:15 -0500

    habilitando para la ejecusion de ver_calificaiones

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.routes.main_routes import main_bp
 #from app.routes.course_routes import curso_bp
 from app.routes.announcement_routes import anuncios_bp
 #from app.routes.qualification_routes import calificaciones_bp
-#from app.routes.notas_routes import notas_bp
+from app.routes.notas_routes import notas_bp
 
 from app.infrastructure.database import init_db, create_tables
 
@@ -25,5 +25,5 @@ def create_app():
     #app.register_blueprint(curso_bp)
     app.register_blueprint(anuncios_bp)
     #app.register_blueprint(calificaciones_bp)
-    #app.register_blueprint(notas_bp)
+    app.register_blueprint(notas_bp)
     return app
