db0ac42bf82eaef3b8253b499620b3059263c5d8
BerlyCS <>
2025-07-28 14:58:53 -0500

    Pagina perfil de usuario

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,9 @@ from app.routes.announcement_routes import anuncios_bp
 #from app.routes.qualification_routes import calificaciones_bp
 from app.routes.auth_routes import auth_bp
 from app.routes.grades_routes import grades_routes
+from app.routes.notas_routes import notas_bp
+from app.routes.user_route import user_bp
+
 from app.infrastructure.database import init_db, create_tables
 
 def create_app():
@@ -28,5 +31,6 @@ def create_app():
     #app.register_blueprint(notas_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(grades_routes)
+    app.register_blueprint(notas_bp)
+    app.register_blueprint(user_bp)
     return app
-1
