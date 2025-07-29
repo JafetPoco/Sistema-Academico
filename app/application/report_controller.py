5342e95e34d07c067f851888c9edda1249720dec
NandUNSAS <fsuca@unsa.edu.pe>
2025-07-28 20:04:52 -0500

    implementacion de controlador y route de reporte de curso

--- app/application/report_controller.py
+++ app/application/report_controller.py
@@ -1,11 +1,9 @@
 from flask import render_template
-from domain.services.report_service import ReportService
+from app.domain.services.report_service import ReportService
+
+report_service = ReportService()
 
 def show_course_report(course_id):
-    report_service = ReportService()
-    report_data = report_service.get_course_grades_report(course_id)
-    
-    if not report_data:
-        return "Course not found", 404
+    course_grades, _ = report_service.get_course_grades(course_id)
+    return render_template('reporte/reporte_curso.html', grades=course_grades)
 
-    return render_template('reporte/reporte.html', report=report_data)
--- app/routes/report_routes.py
+++ app/routes/report_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 from app.application.report_controller import show_course_report
 
-reporte_bp = Blueprint('reporte', __name__, url_prefix='/reporte')
+report_bp = Blueprint('reporte', __name__, url_prefix='/reporte')
 
-@reporte_bp.route('/curso/<int:course_id>', methods=['GET'])
+@report_bp.route('/curso/<int:course_id>', methods=['GET'])
 def course_report(course_id):
     return show_course_report(course_id)
