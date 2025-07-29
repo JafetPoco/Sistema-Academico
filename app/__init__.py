51864168b4ee837be5cc6a966737a48cdbb313bc
NandUNSAS <fsuca@unsa.edu.pe>
2025-07-28 20:08:15 -0500

    implementacion de vista para el reporte de curso

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from app.routes.announcement_routes import anuncios_bp
 #from app.routes.qualification_routes import calificaciones_bp
 #from app.routes.notas_routes import notas_bp
 from app.routes.login_routes import auth_bp
-from app.routes.notas_routes import notas_bp
+#from app.routes.notas_routes import notas_bp
+from app.routes.report_routes import report_bp
 
 from app.infrastructure.database import init_db, create_tables
 
@@ -29,5 +30,6 @@ def create_app():
     #app.register_blueprint(calificaciones_bp)
     #app.register_blueprint(notas_bp)
     app.register_blueprint(auth_bp)
-    app.register_blueprint(notas_bp)
+    #app.register_blueprint(notas_bp)
+    app.register_blueprint(report_bp)
     return app
