ea4b436bb39d496104f136c149cd1356a0d07227
BerlyCS <>
2025-07-29 11:46:29 -0500

    Agregado ruta de usuario

--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -1,17 +1,10 @@
 from flask import Blueprint, redirect, render_template, url_for, session
-from app.application.auth_controller import (
-    do_login,
-    do_register,
-    show_register,
-    show_login,
-    do_logout
-)
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
 
-@user_bp.route("/perfil", methods=["GET"])
-def show_profile():
-    if "user" not in session:
+@user_bp.route("/profile", methods=["GET"])
+def profile():
+    if "user_id" not in session:
         return redirect(url_for("auth.login_get"))
     return render_template("user/profile.html")
 
