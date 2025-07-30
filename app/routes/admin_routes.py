183b5f1305668bff066ec8d1509f0ecb53a391e7
BerlyCS <>
2025-07-29 22:13:45 -0500

    Arreglado admin no puede entrar a usuarios

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template, redirect, url_for, session
 from app.application.admin_controller import AdminController
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
@@ -7,11 +7,16 @@ controller = AdminController()
 
 @admin_bp.route('/users', methods=['GET'])
 def users():
+    print(session.get("role"))
+    if session.get("role") != 2:
+        return redirect(url_for('main.index'))
     users = controller.handle_get_users()
     return render_template('admin/user_list.html', users=users)
 
 @admin_bp.route('/users/<int:user_id>', methods=['POST'])
 def update_user(user_id):
+    if session.get("role") != 2:
+        return redirect(url_for('main.index'))
     role = request.form.get('role')
     if (controller.handle_update_user(user_id, role)):
         return redirect(url_for('admin.users'))
