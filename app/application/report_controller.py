08820536045707a796ab5b6dfc26663fa1b51c53
NandUNSAS <fsuca@unsa.edu.pe>
2025-07-29 21:40:26 -0500

    estableciendo un form para reporte

--- app/application/report_controller.py
+++ app/application/report_controller.py
@@ -1,9 +1,19 @@
-from flask import render_template
+from flask import render_template, session 
+from app.infrastructure.repository.repository import CourseRepository
 from app.domain.services.report_service import ReportService
-
+from app.domain.services.course_service import CourseService
 report_service = ReportService()
+
+course_repository = CourseRepository()
+course_service = CourseService(course_repository)
+
+def show_form_report():
+    user_id = session.get('user_id')
+    courses = course_service.get_courses_by_professor(user_id)
+    return render_template('reporte/formulario_reporte.html', courses=courses)
 
 def show_course_report(course_id):
     course_grades, _ = report_service.get_course_grades(course_id)
-    return render_template('reporte/reporte_curso.html', grades=course_grades)
+    course_name = course_repository.get_course_name_by_id(course_id)
+    return render_template('reporte/reporte_curso.html', grades=course_grades, course_name=course_name)
 
