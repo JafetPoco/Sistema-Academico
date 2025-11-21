6a114e9efeeca5c36bf7f6689ff91baa8643cb6f
Jafet Poco Chire <jpococ@unsa.edu.pe>
2025-11-20 22:06:53 -0500

    Pruebas de funcionalidad - vista principal

---
+++ tests/integration/selenium/test_principal.py
@@ -0,0 +1,79 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_principal_carga(driver):
+    driver.get("http://localhost:5000/")
+
+    # Verificar título
+    title = driver.title
+    assert title == "Inicio - EDUNET"
+
+    # Verificar carga de botones
+    btn_home = driver.find_element(By.ID, "btn-home")
+    btn_anuncios = driver.find_element(By.ID, "btn-anuncios")
+    btn_dashboard = driver.find_element(By.XPATH, "//a[text()='Dashboard']")
+    btn_iniciar_sesion = driver.find_element(By.ID, "btn-iniciar-sesion")
+    btn_mi_perfil = driver.find_elements(By.ID, "btn-mi-perfil")
+
+    assert btn_home.is_displayed()
+    assert btn_anuncios.is_displayed()
+    assert btn_dashboard.is_displayed()
+    assert btn_iniciar_sesion.is_displayed()
+    assert len(btn_mi_perfil) == 0  # No debe estar visible si no está logeado
+
+    # Verificar carga del logo
+    img_principal = driver.find_element(By.XPATH, "//img[@alt='EDUNET']")
+    assert img_principal.is_displayed()
+
+def test_principal_correct_path_sin_logear(driver):
+    driver.get("http://localhost:5000/")
+
+    # Verificacion de botones
+    btn_home = driver.find_element(By.ID, "btn-home")
+    btn_anuncios = driver.find_element(By.ID, "btn-anuncios")
+    btn_dashboard = driver.find_element(By.XPATH, "//a[text()='Dashboard']")
+    btn_iniciar_sesion = driver.find_element(By.ID, "btn-iniciar-sesion")
+
+    assert btn_home.get_attribute("href") == "http://localhost:5000/"
+    assert btn_anuncios.get_attribute("href") == "http://localhost:5000/anuncios/"
+    assert btn_dashboard.get_attribute("href") == "http://localhost:5000/auth/login"
+    assert btn_iniciar_sesion.get_attribute("href") == "http://localhost:5000/auth/login"
+
+def login(driver):
+    driver.get("http://localhost:5000/auth/login")
+    driver.find_element(By.ID, "email").send_keys("admin@prueba.com")
+    driver.find_element(By.ID, "password").send_keys("admin")
+    driver.find_element(By.XPATH, "//button[contains(text(),'Iniciar sesión')]").click()
+
+    WebDriverWait(driver, 10).until(
+        EC.url_contains("http://localhost:5000/dashboard/")
+    )
+
+def test_principal_correct_path_logeado(driver):
+    login(driver)
+
+    # Navegar a la pantalla principal
+    driver.get("http://localhost:5000/")
+
+    #Verificar botones
+    btn_mi_perfil = driver.find_element(By.ID, "btn-mi-perfil")
+    btn_logout = driver.find_element(By.ID, "btn-logout")
+    btn_home = driver.find_element(By.ID, "btn-home")
+    btn_anuncios = driver.find_element(By.ID, "btn-anuncios")
+    btn_dashboard = driver.find_element(By.XPATH, "//a[text()='Dashboard']")
+
+    assert btn_mi_perfil.is_displayed()
+
+    assert btn_home.get_attribute("href") == "http://localhost:5000/"
+    assert btn_anuncios.get_attribute("href") == "http://localhost:5000/anuncios/"
+    assert btn_dashboard.get_attribute("href") == "http://localhost:5000/dashboard/"
+    assert btn_mi_perfil.get_attribute("href") == "http://localhost:5000/user/profile"
+    assert btn_logout.get_attribute("href") == "http://localhost:5000/auth/logout"
+    
+    #Cerrar sesión
+    btn_logout.click()
+    WebDriverWait(driver, 10).until(
+        EC.url_contains("http://localhost:5000/")
+    )
+    assert driver.current_url == "http://localhost:5000/"
